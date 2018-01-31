b4a734595d40c1a39d65ef2ed491936ec316b2a7
Luke Przekwas <galactica@The-Galactica.local>
2018-01-31 15:30:50 -0600

    red x button deletes chirps added

--- client/app.js
+++ client/app.js
@@ -13,12 +13,25 @@ fetch("/api/chirps/")
                 let author = object[id].name;
                 let text = object[id].text;
 
-                let currentCard = $(`<div class='card mt-2'><div class='card-body'><h5 class="card-title">${author}</h5><p class='card-text'>${text}</p></div></div>`);
+                let currentCard = $(`<div class='card mt-2 border border-secondary rounded text-center'><div class='card-body'><h5 class="card-title">${author}</h5><p class='card-text'>${text}</p><button class='delete btn btn-danger' data-id='${id}'>X</button></div></div>`);
                 currentCard.appendTo(chirpDiv);
+                $(".delete").click(delete_item);
             }
         })
     })
     .catch(error => {
         console.log(error);
     })
+
+//function to make an ajax request to DELETE the specific id
+function delete_item() {
+    let id = $(this).attr('data-id');
+    jQuery.ajax({
+        url: '/api/chirps/' + id,
+        type: 'DELETE',
+        success: (data) => {
+            location.href = '/'
+        }
+    });
+}
 
