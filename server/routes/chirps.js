ac1daec1ecfd8e1de948e09806eb0ec254ea5035
Luke Przekwas <galactica@The-Galactica.local>
2018-01-31 11:21:49 -0600

    get, push, put, delete working

---
+++ server/routes/chirps.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const chirpStore = require("../chirpsstore");
+
+let router = express.Router();
+
+router.get('/:id?', (req, res) => {
+    let id = req.params.id;
+    if (id) {
+        res.send(chirpStore.GetChirp(id));
+    } else {
+        res.send(chirpStore.GetChirps());
+    }
+});
+
+router.post('/', (req, res) => {
+    chirpStore.CreateChirp(req.body);
+    res.sendStatus(200);
+});
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    chirpStore.UpdateChirp(id, req.body);
+    res.sendStatus(200);
+});
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    chirpStore.DeleteChirp(id);
+    res.sendStatus(200);
+});
+
+module.exports = router;
